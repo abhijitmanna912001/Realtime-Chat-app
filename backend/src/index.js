01d354e31fb3e1010c92e0fde8e9de9cad8ea061
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-06-26 22:05:04 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,15 +10,16 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use(cookieParser());
-// app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,7 +8,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173"],
+    credentials: true,
   },
+  maxHttpBufferSize: 5e7, // ✅ 50MB
 });
 
 export function getReceiverSocketId(userId) {
@@ -18,15 +20,12 @@ export function getReceiverSocketId(userId) {
 const userSocketMap = {};
 
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
-
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
