ec986668257e0fc3d3fed4e954bee47c8e93c38e
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-06-27 15:11:14 +0530

    Frontend Code changed for deployment issues

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001"
+    : "https://backend-dq1n.onrender.com";
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : import.meta.env.VITE_API_BASE_URL,
+  baseURL: `${BASE_URL}/api`,
   withCredentials: true,
 });
